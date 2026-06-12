964e0d63258913891aa553259087606c8a02a212
Andrzej <bazylek11@gmail.com>
2026-06-12 15:31:56 +0200

    feat: dodaj farewell (commit B2)

--- Greeting.java
+++ Greeting.java
@@ -2,4 +2,8 @@ public class Greeting {
     public static String greet(String name) {
         return "Hello, " + name + "!";
     }
+
+    public static String farewell(String name) {
+        return "Goodbye, " + name + "!";
+    }
 }
