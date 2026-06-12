010e3c67010fc805792916b2d24d043f59c392d6
Andrzej <bazylek11@gmail.com>
2026-06-12 15:31:57 +0200

    feat: dodaj time (commit B3)

--- Greeting.java
+++ Greeting.java
@@ -6,4 +6,10 @@ public class Greeting {
     public static String farewell(String name) {
         return "Goodbye, " + name + "!";
     }
+
+    public static String time(int hour) {
+        if (hour < 12) return "morning";
+        if (hour < 18) return "afternoon";
+        return "evening";
+    }
 }
